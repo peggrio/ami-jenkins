e5526ccb60ccd35d2dca0d7fa10e8b463772b265
Peizhen Liao <peizhenliao23@gmail.com>
2024-05-31 00:51:54 -0400

    install plugins on premises, disable run-setup-wizard

--- packer/install-nginx.sh
+++ packer/install-nginx.sh
@@ -4,5 +4,18 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install nginx -y
 sudo systemctl start nginx
+
+# Configures the Nginx service to start automatically at boot time
+echo "================================="
+echo "Configures the Nginx service to start automatically at boot time"
+echo "================================="
 sudo systemctl enable nginx
+
+# Installs Certbot, which helps in obtaining and installing SSL certificates
+echo "================================="
+echo "Installs Certbot"
+echo "================================="
+sudo apt-get install certbot python3-certbot-nginx -y
+
+
 sudo systemctl status nginx
